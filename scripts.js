3c9444febdfead20547aec91a037ff19534be2d0
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-15 17:45:13 -0800

    create a madlibs webpage

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nountInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nountInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
